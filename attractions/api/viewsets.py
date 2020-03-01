e9b7e147c32c1a6749ca082924ae84d1dd6131cd
ekkopy <thiagognlcn@gmail.com>
2020-02-29 22:27:03 -0300

    Attractions App - Add search on viewsets :filter_backends = [filters.SearchFilter]

--- attractions/api/viewsets.py
+++ attractions/api/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from attractions.models import Attractions
 from attractions.api.serializers import AttractionsSerializer
@@ -8,3 +9,6 @@ class AttractionsViewSet(ModelViewSet):
     """ Viewset das aplicações """
     queryset = Attractions.objects.all()
     serializer_class = AttractionsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('name', 'description')
+    
