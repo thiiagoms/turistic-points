6041b05f4863970e8d728b4148954c44ed1b0c2e
ekkopy <thiagognlcn@gmail.com>
2020-02-29 22:30:19 -0300

    Add filter search on viewsets :sunny:

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,13 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from core.models import TuristicPoints
 from core.api.serializers import TurisiticPointsSerializer
-
 
 # viewset
 class TuristicPointsViewSet(ModelViewSet):
     """ Viewset da API """
     serializer_class = TurisiticPointsSerializer
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
 
     def get_queryset(self):
         return TuristicPoints.objects.filter(approved=True)
